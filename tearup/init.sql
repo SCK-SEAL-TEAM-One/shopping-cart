2e6972c41a0502f2ef3b4aafa6826b9e609eb1b2
endkarn <end_karn@hotmail.com>
2020-03-02 11:44:58 +0700

    [Added]: SQL สำหรับเพิ่มข้อมูลของเล่นไอดีที่ 2 ใน Table product

--- tearup/init.sql
+++ tearup/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE user (
-    ID int,
-    Name varchar(255)
+    id int,
+    name varchar(255)
 );
 
 INSERT INTO user VALUE (1,"sckshuhari");
@@ -17,3 +17,5 @@ CREATE TABLE product (
     created timestamp DEFAULT current_timestamp ON UPDATE current_timestamp,
     PRIMARY KEY (id)
 );
+
+INSERT INTO product(id,product_name,product_brand,quantity,product_price,image_url) VALUE (2,"43 Piece dinner Set","CoolKidz",10,12.95,"/43_Piece_dinner_Set.png");
