ea93bc75bce41cb24f27167e8e70f5869a888641
netty <netty.ncz@gmail.com>
2020-03-02 14:16:29 +0700

    [Added]: Function GetTotalProductPrice

--- store-service/internal/order/order.go
+++ store-service/internal/order/order.go
@@ -1,5 +1,7 @@
 package order
 
+import "store-service/internal/product"
+
 type OrderInterface interface {
 	CreateOrder(totalPrice float64) (int, error)
 	CreatedOrderProduct(orderID int, listProduct []OrderProduct) error
@@ -12,4 +14,13 @@ type OrderService struct {
 
 type ProductRepository interface {
 	GetProductByID(id int) product.Product
+}
+
+func (orderService OrderService) GetTotalProductPrice(submitedOrder SubmitedOrder) float64 {
+	totalProductPrice := 0.00
+	for _, cartItem := range submitedOrder.Cart {
+		product := orderService.ProductRepository.GetProductByID(cartItem.ProductID)
+		totalProductPrice += product.Price * float64(cartItem.Quantity)
+	}
+	return totalProductPrice
 }
