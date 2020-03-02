d9bf9a8aeb5de1b3f205ce378dd9982659e4a2f6
Beautiievil <gossippiiz@gmail.com>
2020-03-02 12:03:04 +0700

    [Edited] : ย้าย Struct ขื่อ SubmmitedOrder ไปไว้ที่ internal/order/model.go

--- store-service/cmd/api/order.go
+++ store-service/cmd/api/order.go
@@ -12,17 +12,6 @@ import (
 type StoreAPI struct {
 	OrderDB order.OrderInterface
 }
-type SubmmitedOrder struct {
-	Cart                 []order.OrderProduct `json:"cart"`
-	ShippingMethod       int                  `json:"shipping_method"`
-	ShippingAddress      string               `json:"shipping_address"`
-	ShippingSubDistrict  string               `json:"shipping_sub_disterict"`
-	ShippingDistrict     string               `json:"shipping_district"`
-	ShippingProvince     string               `json:"shipping_province"`
-	ShippingZipCode      string               `json:"shipping_zip_code"`
-	RecipientName        string               `json:"recipient_name"`
-	RecipientPhoneNumber string               `json:"recipient_phone_number"`
-}
 
 type OrderConfirmation struct {
 	OrderID    int     `json:"order_id"`
