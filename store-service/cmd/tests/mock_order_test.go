10775e61338d2d2ac770d325081462e494544af5
Beautiievil <gossippiiz@gmail.com>
2020-03-02 16:49:13 +0700

    [Edited]: เอา mock ที่ต่อกับ database ออกและเรียกใช้ของตัว service แทน คือ CreateOrder ที่ cmd/api/test/mock_order_test.go

--- store-service/cmd/tests/mock_order_test.go
+++ store-service/cmd/tests/mock_order_test.go
@@ -10,17 +10,7 @@ type mockOrderDB struct {
 	mock.Mock
 }
 
-func (order *mockOrderDB) CreateOrder(totalPrice float64) (int, error) {
-	argument := order.Called(totalPrice)
-	return argument.Int(0), argument.Error(1)
-}
-
-func (order *mockOrderDB) CreatedOrderProduct(orderID, productID, quantity int, productPrice float64) error {
-	argument := order.Called(orderID, productID, quantity, productPrice)
-	return argument.Error(0)
-}
-
-func (order *mockOrderDB) CreatedShipping(orderID int, shippingInfo order.ShippingInfo) error {
-	argument := order.Called(orderID, shippingInfo)
-	return argument.Error(0)
+func (orderService *mockOrderDB) CreateOrder(submitedOrder order.SubmitedOrder) order.Order {
+	argument := orderService.Called(submitedOrder)
+	return argument.Get(0).(order.Order)
 }
