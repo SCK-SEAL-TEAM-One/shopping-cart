e4d410671c9bc5a3e23c0409e48dd16c57552e4e
endkarn <end_karn@hotmail.com>
2020-03-02 11:31:52 +0700

    [Added]: SQL ของการสร้าง Table Product

--- tearup/init.sql
+++ tearup/init.sql
@@ -4,3 +4,16 @@ CREATE TABLE user (
 );
 
 INSERT INTO user VALUE (1,"sckshuhari");
+
+
+CREATE TABLE product (
+    id int AUTO_INCREMENT,
+    product_name varchar(255),
+    product_brand varchar(255),
+    quantity int,
+    product_price double,
+    image_url varchar(255),
+    updated timestamp DEFAULT current_timestamp,
+    created timestamp DEFAULT current_timestamp ON UPDATE current_timestamp,
+    PRIMARY KEY (id)
+);
