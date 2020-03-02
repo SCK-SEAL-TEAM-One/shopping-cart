d5c8f3edebe180bc2284c408d0d1f49150fe758d
Beautiievil <gossippiiz@gmail.com>
2020-03-02 15:07:13 +0700

    [Created]:function Create Order ใน internal/order/order.go

--- store-service/internal/order/order.go
+++ store-service/internal/order/order.go
@@ -1,7 +1,54 @@
 package order
 
-type OrderInterface interface {
-	CreateOrder(totalPrice float64) (int, error)
-	CreatedOrderProduct(orderID int, listProduct []OrderProduct) error
-	CreatedShipping(orderID int, shippingInfo ShippingInfo) error
+import (
+	"log"
+	"store-service/internal/product"
+)
+
+type OrderService struct {
+	ProductRepository product.ProductRepository
+	OrderRepository   OrderInterface
+}
+
+func (service OrderService) CreateOrder(submitedOrder SubmitedOrder) Order {
+	var orderProduct OrderProduct
+
+	productID, err := service.ProductRepository.GetProductByID(orderProduct.ProductID)
+	if err != nil {
+		log.Printf("GetProductByID internal error %s", err.Error())
+		return Order{}
+	}
+
+	totalAmount := GetTotalAmount(submitedOrder)
+
+	orderID, err := service.OrderRepository.CreateOrder(totalAmount)
+	if err != nil {
+		log.Printf("OrderRepository.CreateOrder internal error %s", err.Error())
+		return Order{}
+	}
+
+	shippingInfo := ShippingInfo{
+		ShippingMethod:       submitedOrder.ShippingMethod,
+		ShippingAddress:      submitedOrder.ShippingAddress,
+		ShippingSubDistrict:  submitedOrder.ShippingSubDistrict,
+		ShippingDistrict:     submitedOrder.ShippingDistrict,
+		ShippingProvince:     submitedOrder.ShippingProvince,
+		ShippingZipCode:      submitedOrder.ShippingZipCode,
+		RecipientName:        submitedOrder.RecipientName,
+		RecipientPhoneNumber: submitedOrder.RecipientPhoneNumber,
+	}
+	err = service.OrderRepository.CreatedShipping(orderID, shippingInfo)
+	if err != nil {
+		log.Printf("OrderRepository.CreatedShipping internal error %s", err.Error())
+		return Order{}
+	}
+
+	for _, selectedProduct := range submitedOrder.Cart {
+		err = service.OrderRepository.CreatedOrderProduct(orderID, selectedProduct.ProductID, selectedProduct.Quantity, selectedProduct.ProductPrice)
+		if err != nil {
+			log.Printf("OrderRepository.CreatedOrderProduct internal error %s", err.Error())
+			return Order{}
+		}
+	}
+	return Order{}
 }
