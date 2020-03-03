7208fe128f095c71a11376ce958ba3ea41fd4c1e
platylm <n_nareenart@kkumail.com>
2020-03-03 15:29:18 +0700

    [Added]:mock ของ SendNotification มาที่ cmd/test/mock_order_test.go

--- store-service/cmd/tests/mock_order_test.go
+++ store-service/cmd/tests/mock_order_test.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"store-service/internal/order"
+	"time"
 
 	"github.com/stretchr/testify/mock"
 )
@@ -13,4 +14,9 @@ type mockOrderService struct {
 func (orderService *mockOrderService) CreateOrder(submitedOrder order.SubmitedOrder) order.Order {
 	argument := orderService.Called(submitedOrder)
 	return argument.Get(0).(order.Order)
+}
+
+func (orderService *mockOrderService) SendNotification(orderID, trackingID int, dateTime time.Time, shippingMethod string) string {
+	argument := orderService.Called(orderID, trackingID, dateTime, shippingMethod)
+	return argument.String(0)
 }
