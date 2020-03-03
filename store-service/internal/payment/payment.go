93811d3f74f322a05d2a6f6627572920f10f6f60
platylm <n_nareenart@kkumail.com>
2020-03-03 12:28:11 +0700

    [Created]: function ConfirmPayment (ที่ return string hard code ไว้อยู่) ที่ internal/payment/payment.go

--- store-service/internal/payment/payment.go
+++ store-service/internal/payment/payment.go
@@ -3,3 +3,7 @@ package payment
 type PaymentInterface interface {
 	ConfirmPayment(paymentdetail PaymentDetail) string
 }
+
+func ConfirmPayment(paymentdetail PaymentDetail) string {
+	return "วันเวลาที่ชำระเงิน 1/3/2563 13:30:00 หมายเลขคำสั่งซื้อ 8004359103 คุณสามารถติดตามสินค้าผ่านช่องทาง Kerry หมายเลข Tracking 1785261900"
+}
