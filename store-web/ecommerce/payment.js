bbe0518944520ac81f77450ac90f63014c79fc95
Beautiievil <gossippiiz@gmail.com>
2020-03-03 11:42:53 +0700

    [Edited]: Implement Function ให้ผ่านตาม Test Condition (VISA)

--- store-web/ecommerce/payment.js
+++ store-web/ecommerce/payment.js
@@ -1,5 +1,10 @@
 export default function checkPaymentMethod(cardNumber) {
-    if (cardNumber.startsWith('5')) {
-        return "MASTER"
-    }
+  if (cardNumber.startsWith('5')) {
+    return 'MASTER'
+  }
+
+  if (cardNumber.startsWith('4')) {
+    return 'VISA'
+  }
+
 }
