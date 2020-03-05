5913e6d48734cde3bb63d23de739d5e4ac32306a
endkarn <end_karn@hotmail.com>
2020-03-05 12:14:29 +0700

    [Added]: Interface ของ ShippingRepository

--- store-service/internal/shipping/repository.go
+++ store-service/internal/shipping/repository.go
@@ -6,12 +6,16 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+type ShippingRepository interface {
+	GetShippingByOrderID(orderID int) (order.ShippingInfo, error)
+}
+
 type ShippingRepositoryMySQL struct {
 	DBConnection *sqlx.DB
 }
 
 func (repository ShippingRepositoryMySQL) GetShippingByOrderID(orderID int) (order.ShippingInfo, error) {
 	var shippingInfo order.ShippingInfo
-	err := repository.DBConnection.Get(&shippingInfo, "SELECT orders.shipping_method as method, address, sub_district, district, province, zip_code, recipient, phone_number FROM shipping INNER JOIN orders ON shipping.order_id = orders.id WHERE order_id = ?", orderID)
+	err := repository.DBConnection.Get(&shippingInfo, "SELECT orders.shipping_method as method, address, sub_district, district, province, zip_code, recipient, phone_number FROM shipping INNER  orders ON shipping.order_id = orders.id WHERE order_id = ?", orderID)
 	return shippingInfo, err
 }
