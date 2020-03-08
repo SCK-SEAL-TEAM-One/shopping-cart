334084ada9e6cdf8d380b97143c06477dc4776b5
platylm <n_nareenart@kkumail.com>
2020-03-08 16:50:36 +0700

    [Edited]:เพิ่ม log error เข้ามาในแต่ละ function ที่เรียกที่ internal/payment/payment.go

--- store-service/internal/payment/payment.go
+++ store-service/internal/payment/payment.go
@@ -1,6 +1,7 @@
 package payment
 
 import (
+	"log"
 	"store-service/internal/order"
 	"store-service/internal/product"
 	"store-service/internal/shipping"
@@ -31,28 +32,34 @@ type PaymentService struct {
 func (service PaymentService) ConfirmPayment(orderID int, paymentdetail PaymentDetail) string {
 	transactionId, err := service.BankGateway.Payment(paymentdetail)
 	if err != nil {
+		log.Printf("BankGateway.Payment internal error %s", err.Error())
 		return ""
 	}
 	orderProductList, err := service.OrderRepository.GetOrderProduct(orderID)
 	if err != nil {
+		log.Printf("OrderRepository.GetOrderProduct internal error %s", err.Error())
 		return ""
 	}
 	for _, orderProduct := range orderProductList {
 		err = service.ProductRepository.UpdateStock(orderProduct.ProductID, orderProduct.Quantity)
 		if err != nil {
+			log.Printf("ProductRepository.UpdateStock internal error %s", err.Error())
 			return ""
 		}
 	}
 	shippingInfo, err := service.ShippingRepository.GetShippingByOrderID(orderID)
 	if err != nil {
+		log.Printf("ShippingRepository.GetShippingByOrderID internal error %s", err.Error())
 		return ""
 	}
 	trackingID, err := service.ShippingGateway.ShipByKerry(shippingInfo)
 	if err != nil {
+		log.Printf("ShippingGateway.ShipByKerry internal error %s", err.Error())
 		return ""
 	}
 	err = service.OrderRepository.UpdateOrder(orderID, transactionId)
 	if err != nil {
+		log.Printf("OrderRepository.UpdateOrder internal error %s", err.Error())
 		return ""
 	}
 
