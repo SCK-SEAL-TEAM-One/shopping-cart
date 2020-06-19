9aa1825d0e119a887ec134fddb567e29cd8df4b7
smalldog124 <panumars124@gmail.com>
2020-06-19 09:58:55 +0700

    [Edited]: แก้ไขที่ของรูปภาพที่หน้า Product-list

--- store-web/pages/Product-list.js
+++ store-web/pages/Product-list.js
@@ -5,15 +5,18 @@ import Route from 'next/router'
 
 
 export default class ProductList extends React.Component {
+  getProductDetail(){
+    Route.push('/Product-detail')
+  }
   render() {
     return (
       <Container>
         <table>
             <tr>
               <dev>
-              <img src="https://i.pinimg.com/736x/6c/38/ab/6c38abb1475ac373ee2d9d78da609cb3--fine-porcelain-dinner-sets.jpg" width="20%"></img>
+              <img src="https://i.pinimg.com/474x/17/43/2f/17432f12ec88c0d0ea3d0cffc69d25ce.jpg" width="20%"></img>
               </dev>
-              <div>
+              <div onClick={()=> this.getProductDetail()}>
                 <h3 id="productName-1">43 Piecee Dinner Set</h3>
                 <h5 id="productPrice-1">12.95 USD</h5>
               </div>
