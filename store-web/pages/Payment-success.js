8e2b17c66dd583567d4871e6dae611cb966a1497
Apipol Sukgler <golf.apipol@gmail.com>
2020-09-08 13:28:40 +0700

    [Edited]: ย้าย message ไว้ใน function

--- store-web/pages/Payment-success.js
+++ store-web/pages/Payment-success.js
@@ -4,6 +4,10 @@ import Route from 'next/router'
 
 
 export default class PaymentSucess extends React.Component {
+  componentDidUpdate() {
+    this.message = 'วันเวลาที่ชำระเงิน 1/3/2563 13:30:00 หมายเลขคำสั่งซื้อ 8004359103 คุณสามารถติดตามสินค้าผ่านช่องทาง Kerry ด้วยหมายเลข 1785261900'
+  }
+
   goHome() {
     Route.push('/Product-list')
   }
@@ -15,8 +19,7 @@ export default class PaymentSucess extends React.Component {
           <h1 id="title">ชำระเงินสำเร็จ</h1>
           <Image src="https://www.pngitem.com/pimgs/m/69-692608_transparent-answer-icon-png-check-pass-icon-png.png" width="8%" />
           <div id="notify">
-            วันเวลาที่ชำระเงิน 1/3/2563 13:30:00 หมายเลขคำสั่งซื้อ 8004359103
-            คุณสามารถติดตามสินค้าผ่านช่องทาง Kerry ด้วยหมายเลข 1785261900
+            {this.message}
           </div>
           <Button id="goHome" onClick={() => this.goHome()}>กลับไปหน้าหลัก</Button>
         </div>
